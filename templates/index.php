5ff37a50e54fb71728b59ccb44b143bdea4c57cd
Claudia Lim <climweiching@gmail.com>
2023-11-17 12:30:54 +0000

    initial styling

--- templates/index.php
+++ templates/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8"/>
     <title>To Do App</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+    <link href="styles.css" rel="stylesheet">
 </head>
 <body>
 <h1>To Do App</h1>
